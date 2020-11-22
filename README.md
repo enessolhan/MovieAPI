31e6821c2f7a6642fd417f69a87614a1ad4b83c0
enessolhan <enessolhan@gmail.com>
2020-11-22 14:13:15 +0300

    Movie API Project
    You can search any move just typing their name, so it will give you the 10 related movie with your searched word.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie API Project
+
+#### You can search any move just typing their name, so it will give you the 10 related movie with your searched word. 
---
+++ app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("#searchform");
+form.addEventListener("submit", async function(e){
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    makeImages(response.data);
+    form.elements.query.value = "";
+});
+
+const makeImages = (shows) =>{
+    for (let result of shows) {
+        if(result.show.image){
+            const img = document.createElement("IMG");
+            img.src=result.show.image.medium;
+            document.body.append(img);
+        }
+        
+    }
+}
